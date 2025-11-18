4b0d0c8edeecd0d5ff4d1294919acc9db1bfc7d8
ethanwait25 <ethanwait25@gmail.com>
2025-11-18 14:19:52 -0700

    Set up simple express server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,18 @@
+import express from "express";
+
 console.log("Server starting up...")
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("Welcome to echo.");
+});
+
+app.get('/hello', (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
